5e008dded927ef83a78b31ca4e5bfe5897ced437
jassonborg <jasson.borg@gmail.com>
2024-05-29 11:44:02 +1200

    Pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(str, count) {
     let result = ''
+    let error = 'ERROR'
     for(let i = 0; i < count; i++){
         result += str;
     }
-return result;
+    if(count < 0) {
+        return error;
+    }
+    return result;
 };
 
 // Do not edit below this line
