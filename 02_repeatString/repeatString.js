27826f9a68411e2ede897cf1e776a0def3754d84
jassonborg <jasson.borg@gmail.com>
2024-05-29 11:00:34 +1200

    Pass repeats the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let result = ''
+    for(let i = 0; i < count; i++){
+        result += str;
+    }
+return result;
 };
 
 // Do not edit below this line
