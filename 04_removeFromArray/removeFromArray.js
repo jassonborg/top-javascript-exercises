2c57301174ffca946f6d7138f3d35674a0d34101
jassonborg <jasson.borg@gmail.com>
2024-05-30 14:29:28 +1200

    Pass removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
+let myArray = [1, 2, 3, 4, 7, "tacos"];
+
+const removeFromArray = (myArray, ...args) => {
+
+    const newArray = [...myArray];
+    const uniqueArray = [...new Set(args)];
+
+    for(const value of uniqueArray){
+
+        let index = newArray.indexOf(value);
+
+        while(index !== -1){
+            newArray.splice(index, 1);
+            index = newArray.indexOf(value);
+        }
+    }
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
