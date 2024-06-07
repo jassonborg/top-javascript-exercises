ec45ea5774a1ecb87bf06e045911edd899e07e0a
jassonborg <jasson.borg@gmail.com>
2024-06-07 12:42:47 +1200

    Passed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Celsius) {
+  const convertC = (Celsius - 32) * 5 / 9;
+  const roundUp = Math.round(convertC * 10) / 10;
+  return roundUp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Fahrenheit) {
+  const convertF = (Fahrenheit * 9 / 5) + 32;
+  const roundUp = Math.round(convertF * 10) / 10;
+  return roundUp;
 };
 
 // Do not edit below this line
