b2ed6ebbc7a32f799a37dfaa8c1e9a4dd7d823e3
jassonborg <jasson.borg@gmail.com>
2024-06-07 14:20:54 +1200

    Passed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const myArray = [];
+
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  //total shouldn't be 0 otherwise result will be 0
+  let total = 1;
+  
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (let i = 2; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
