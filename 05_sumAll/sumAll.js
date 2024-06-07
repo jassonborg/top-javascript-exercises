6a10016686fe15db3e88a2e0ed8a030135c6fa10
jassonborg <jasson.borg@gmail.com>
2024-06-07 12:11:11 +1200

    Passed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = (start, end) => {
+    //if values are negative
+    if (start <= 0 || end <= 0) {
+        return "ERROR";
+    }
 
+    //if values don't equal to a number
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR";
+    }
+    
+    //swap start and end values
+    if (start > end){
+        [start, end] = [end, start];
+    }
+
+    // 1 to 100 has 100 numbers + 1 (to include the numbers 1 and 100) *
+    // add start and end then / 2
+    return (end - start + 1) * (start + end) / 2;
 };
 
 // Do not edit below this line
