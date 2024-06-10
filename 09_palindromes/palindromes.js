b37328998c38c3f6073370ba18b40eb67b05af16
jassonborg <jasson.borg@gmail.com>
2024-06-10 12:06:53 +1200

    Passed palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    let lowerCase = string.toLowerCase();
+    let result = lowerCase.replace(/[^a-z0-9]/g, '');
+    let reversed = result.split('').reverse().join('');
 
+    return result === reversed;
 };
 
 // Do not edit below this line
